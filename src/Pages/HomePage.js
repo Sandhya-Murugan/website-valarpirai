f962648b84b8db3cb194e40a616374e2cfb073f9
sandhya <sandhyamurugan25.in@gmail.com>
2023-09-30 14:05:12 +0530

    deploying

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -6,12 +6,20 @@ import {Content, Header} from "antd/es/layout/layout";
 import SearchBar from "../Components/SearchBar";
 import OfferZone from "../Components/OfferZone";
 import {OfferZoneData} from "../Assets/OfferZoneData";
-
+import imageJSONData from "../data.json";
 const HomePage = () => {
     const screens = useBreakpoint();
     const isMobile = !screens.md;
     return (
         <div>
+            <div>
+                {imageJSONData.map((image) => (
+                    <div key={image.id}>
+                        <h2>{image.title}</h2>
+                        <img src={image.imagePath} alt={image.title} />
+                    </div>
+                ))}
+            </div>
             <div>
                 <SearchBar />
                 <OfferZone data = {OfferZoneData} /><br/>
